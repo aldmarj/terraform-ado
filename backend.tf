adb416e0810f4477ebd4ccf0da9c7e88379c7cfa
Aldmar <aldmarj@users.noreply.github.com>
2024-09-24 11:55:18 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,10 @@
 terraform {
+ required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.56.0"
+    }
+
   backend "azurerm" {
       resource_group_name  = "ajoubert-aca-rg"
       storage_account_name = "acatfstorage"
